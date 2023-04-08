cb24912f0d780dba177715e14486b95eab22907b
Pratheek Shetty <68729469+gosLp@users.noreply.github.com>
2023-04-09 00:28:16 +0530

    done with cachine tutorial

--- commands.md
+++ commands.md
@@ -11,7 +11,9 @@ redis-cli
 ## some important cli commands
 
 keys myprefix*
+
 keys *pattern*
+
 keys *mysuffix
 
 
@@ -19,11 +21,17 @@ keys *mysuffix
 dump **key** Serialize key
 
 del **key** Delete key
+
 dump **key** Serialize key
+
 exists **key** Check for key
+
 **expire key seconds** Set key TTL
+
 get **key**
+
 set **key** **value**
+
 setnx **key** **value** Set key value only if key does not exist
 
 redis-cli INFO
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,80 @@
 import express from 'express';
 import session from 'express-session';
+import Redis from 'ioredis';
+import  connectRedis  from 'connect-redis';
 
+export type People = {
+    id: number,
+    name: string,
+}[];
 
 const app = express();
+const RedisStore = connectRedis(session);
+let redisClient = new Redis();
+
+
+
 
 
 app.use(session({
-    // store: new RedisStore({}),
+    store:  new RedisStore({ client: redisClient }),
     secret:"your-secret-key",
     resave: false,
     saveUninitialized: false,
 }));
+
+app.get("/people", (req, res) => {
+    redisClient.get("people", (err, data) => {
+        if(err) throw err;
+        if(data) {
+            console.log("Fetching data from the cache");
+            //If data is in the cache, send it to the client
+            res.send(JSON.parse(data));
+            
+        } else {
+            //If data is not in the cache, fetch it from the database and send it to the client
+            console.log("Fetching data from the database");
+            
+            const people = [
+                {id: 1, name: "John"},
+                {id: 2, name: "Doe"},
+                {id: 3, name: "Jane"},
+            ];
+            redisClient.setex("people",3600, JSON.stringify(people));
+            //send fetched data to the client//setinterval to make it seem like the api takes more time than it does
+            // setInterval(() => {
+                
+            //     res.send(people);
+            // }, 1000);
+
+            res.send(people);
+
+        }
+    });
+   
+});
+
+app.get("/people/:id", async (req, res) => {
+    const id = req.params.id;
+
+
+    //Check if the data is in the cache ie. Redis
+    await redisClient.hgetall(`people?id=${id}`, async (err, data) => {
+        if(err) throw err;
+        if(data !== null) {
+            //If data is in the cache, send it to the client
+            console.log("Fetching data from the cache");
+            res.send(data);
+        } else {
+            //If data is not in the cache, fetch it from the database and send it to the client
+            console.log("Fetching data from the database");
+            const person = {  id: id, name: "John" };
+            await redisClient.hmset(`people?id=${id}`, person);
+            res.send(person);
+        }
+    });
+});
+
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
