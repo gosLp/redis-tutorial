dd4a92ee48eedf22cf7ded928d9d88c71b8c7b04
Pratheek Shetty <68729469+gosLp@users.noreply.github.com>
2023-04-08 17:00:50 +0530

    Installed express and start a server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import express from 'express';
+import session from 'express-session';
+
+
+const app = express();
+
+
+app.use(session({
+    // store: new RedisStore({}),
+    secret:"your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
